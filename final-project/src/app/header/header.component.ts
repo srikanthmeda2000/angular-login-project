5da422b6789c4445b2dafa1af7107bd3b7794e25
srikanthmeda2000 <smeda0197@gmail.com>
2023-04-25 09:48:31 +0530

    first commit

---
+++ final-project/src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  constructor(private router:Router , private auth: AuthService){}
+
+  ngOnInit(){
+
+  }
+  goToHome(){
+    this.router.navigate(['home']);
+  }
+  logout(){
+    this.auth.logout();
+  }
+}
---
+++ final-project/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+username ="";
+password ="";
+errorMsg ="";
+constructor(private auth: AuthService , private router:Router){}
+
+ngOnInit() {
+
+}
+login(){
+  if(this.username.trim().length === 0){
+this.errorMsg =" username is required";
+  }else if(this.password.trim().length === 0){
+    this.errorMsg =" password is required";
+      }else{
+        this.errorMsg ="";
+        let res = this.auth.login(this.username, this.password);
+        if(res === 200){
+          this.router.navigate(['home'])
+        }
+        if(res === 403){
+          this.errorMsg='Invalid cradentials';
+        }
+      }
+}
+}
